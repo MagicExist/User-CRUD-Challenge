62630d309c078ccdb062a5312fce0de05b6b1f12
MagicExist <johhanparra1@gmail.com>
2025-05-25 10:42:46 -0500

    (fix): route register now doest need auth

--- Server/Back-End/routes/api.php
+++ Server/Back-End/routes/api.php
@@ -6,13 +6,13 @@ use App\Http\Controllers\UserController;
 use App\Http\Controllers\AuthController;
 
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/users', [UserController::class, 'store']);
 
 Route::middleware('auth:sanctum')->group(function () 
 {
     Route::post('/logout', [AuthController::class, 'logout']);
     Route::get('/users', [UserController::class, 'index']);
     Route::get('/users/{user}', [UserController::class, 'show']);
-    Route::post('/users', [UserController::class, 'store']);
     Route::put('/users/{user}', [UserController::class, 'update']);
     Route::patch('/users/{user}', [UserController::class, 'update']);
     Route::delete('/users/{user}', [UserController::class, 'destroy']);
